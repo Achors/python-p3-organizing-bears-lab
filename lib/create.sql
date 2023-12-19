7ccfd02fc333306928535e3a16b9b260b2f6f97c
Adolf <adolfnyerere@gmail.com>
2023-12-19 14:10:53 +0300

    completed

---
+++ lib/create.sql
@@ -0,0 +1,11 @@
+
+
+CREATE TABLE bears (
+  id Integer,
+  name text,
+  age Integer,
+  sex text,
+  color test,
+  temperament text,
+  alive boolean
+)
---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
+(1, "Mr. Chocolate", 3, "M", "white", "Melancholic", true)
+(2, "Rowdy", 1, "F", "Brown", "plegmatic", false)
+(3, "Tabitha", 4, "M", "Black", "Melancholic", true)
+(4, "Sergeant Brown", 2, "F", "Brown", "plegmatic", false)
+(5, "Melissa", 2, "M", "white", "Melancholic", true)
+(6, "Grinch", 3, "F", "Brown", "Melancholic", true)
+(7, "Wendy", 3, "F", "Black", "choreanic", false)
